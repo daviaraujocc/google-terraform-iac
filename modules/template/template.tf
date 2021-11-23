1a46ae9a86d6da82a8cf3eef96e386967a6a796d
DaviAraujoCC <davi.araujo13356@gmail.com>
2021-11-23 15:55:52 -0300

    create instance template module

---
+++ modules/template/template.tf
@@ -0,0 +1,34 @@
+resource "google_compute_instance_template" "template_default" {
+  name        = var.instance_template_name
+  description = "Templated created for instance group web app"
+
+  tags = var.tags
+  labels = var.labels
+
+  instance_description = "description assigned to instances"
+  machine_type         = var.instance_template_machine_type
+  can_ip_forward       = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  disk {
+    source_image      = var.instance_template_image
+    auto_delete       = true
+    boot              = true
+    disk_type = var.instance_template_disk_type
+    disk_size_gb = var.instance_template_disk_size
+  }
+
+  metadata_startup_script {
+    content = "sudo apt install nginx -y ; echo 'Welcome from host: $(hostname)' > /usr/share/nginx/www/index.html "
+  }
+
+
+  network_interface {
+    network = var.vpc_network_name
+  }
+  
+}
